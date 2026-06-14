936da74231d1125075f8c52c4fb54dc4750792dc
Florian Piesche <florian@yellowkeycard.net>
2026-06-14 16:21:39 +0100

    Remove duplicate definition of saveConfig

--- src/common/scripting/interface/vmnatives.cpp
+++ src/common/scripting/interface/vmnatives.cpp
@@ -1074,17 +1074,6 @@ DEFINE_ACTION_FUNCTION(_CVar, FindCVar)
 	ACTION_RETURN_POINTER(FindCVar(name.GetChars(), nullptr));
 }
 
-static int SaveConfig()
-{
-	return M_SaveDefaults(nullptr);
-}
-
-DEFINE_ACTION_FUNCTION_NATIVE(_CVar, SaveConfig, SaveConfig)
-{
-	PARAM_PROLOGUE;
-	ACTION_RETURN_INT(M_SaveDefaults(nullptr));
-}
-
 //=============================================================================
 //
 //
